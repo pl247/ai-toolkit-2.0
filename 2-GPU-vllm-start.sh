6c125ed38c2508edfa8013c26e696a7e35daf2fb
pl247 <p@home.com>
2025-03-20 09:27:12 -0500

    updated ai-toolkit

--- 2-GPU-vllm-start.sh
+++ 2-GPU-vllm-start.sh
@@ -6,4 +6,4 @@ clear
 echo "Starting vLLM server with OpenAI compatible API on $ip port 8000"
 echo "Going to grab 2 GPUs..."
 echo -e "\n\n\n"
-python -m vllm.entrypoints.openai.api_server --model /ai/models/Meta-Llama-3-8B-Instruct/ --api-key LLM --host $ip --port 8000 --tensor-parallel-size 2 --dtype=half
+python -m vllm.entrypoints.openai.api_server --model /ai/models/NousResearch/Meta-Llama-3-8B-Instruct/ --api-key LLM --host $ip --port 8000 --tensor-parallel-size 2 --dtype=half
